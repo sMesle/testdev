c2b9f8acd8467109cc266ede76d4ebb720a2aeb4
sMesle <steve.demeulemeester@efrei.net>
2019-01-03 23:21:28 +0100

    edit :penguin:

--- controllers/product.js
+++ controllers/product.js
@@ -16,6 +16,29 @@ exports.list = function (req, res) {
   });
 };
 
+exports.view_edit = function (req, res) {
+  Product.findById(req.params.id, function (err, product) {
+    if (err) return next(err);
+    res.render("productedit.ejs", {
+      product: product,
+      moment: moment
+    });
+  });
+};
+
+exports.view_edit_product = function (req, res) {
+  Product.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    type: req.body.type,
+    etat: req.body.etat,
+    description: req.body.description,
+    date: req.body.date
+  }, function (err) {
+    if (err) return next(err);
+    res.redirect("/products");
+  });
+};
+
 exports.product_create = function (req, res) {
   var product = new Product({
     name: req.body.name,
